8afb04bc99556ed82ab9638d89efc4c8ddb9d1cd
Kaleb Haslam <kalebmarc@hotmail.com>
2019-09-03 06:23:40 +0000

    commented code a bit

--- express_server.js
+++ express_server.js
@@ -12,7 +12,11 @@ const urlDatabase = {
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+/**
+ * Generate a unique ID
+ * @param {number} length the length of the string you want
+ * @returns a random ID using chars a-z and 0-9
+ */
 const generateUID = length => {
   const chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
   let returnString = '';
@@ -23,23 +27,31 @@ const generateUID = length => {
   return returnString;
 };
 
+// ===============================================
+// ||               Get requests                ||
+// ===============================================
+// website root
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+//url object as json string
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+//urls page. lists all urls, gives option to edit/delete
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
+//create a new url
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+//shows specific url, lets user edit it from here
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
@@ -47,18 +59,27 @@ app.get('/urls/:shortURL', (req, res) => {
   };
   res.render('urls_show', templateVars);
 });
+
+//redirect user to full url
+app.get('/u/:url', (req, res) => {
+  res.redirect(urlDatabase[req.params.url]);
+});
+
+// ===============================================
+// ||               Post requests               ||
+// ===============================================
+// deletes url
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
 });
+
+//edits existing url
 app.post('/urls/:id', (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect('/urls');
 });
 
-app.get('/u/:url', (req, res) => {
-  res.redirect(urlDatabase[req.params.url]);
-});
 
 app.post('/urls', (req, res) => {
   const uid = generateUID(6);
@@ -67,6 +88,7 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${uid}`);
 });
 
+//starts listening
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
