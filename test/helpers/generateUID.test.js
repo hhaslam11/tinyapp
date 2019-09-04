1f7524882d00b3d9c0e1bec3ecff19df1df34938
Kaleb Haslam <kalebmarc@hotmail.com>
2019-09-04 21:11:14 +0000

    wrote unit tests for generateUID.js

---
+++ test/helpers/generateUID.test.js
@@ -0,0 +1,21 @@
+const assert = require('chai').assert;
+
+const generateUID = require('../../helpers/generateUID').generateUID;
+
+describe('generateUID()', () => {
+  it('should generate string with the length of 5 when passed in 5', () => {
+    assert.isTrue(generateUID(5).length === 5);
+  });
+  it('should return undefined when not passed any value', () => {
+    assert.isUndefined(generateUID());
+  });
+  it('should return undefined when passed a negative value', () => {
+    assert.isUndefined(generateUID(-5));
+  });
+  it('should return undefined when passed a value that isnt a number', () => {
+    assert.isUndefined(generateUID('five'));
+  });
+  it('should return undefined when passed a float', () => {
+    assert.isUndefined(generateUID(5));
+  });
+});
