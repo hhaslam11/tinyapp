da1f139e402ff89f2b7394f1097609296ab4c0bc
hhaslam11 <kalebmarc@hotmail.com>
2019-09-08 10:03:02 -0700

    Updated tests for for the tracking code

--- test/models/urls.test.js
+++ test/models/urls.test.js
@@ -6,16 +6,43 @@ describe('models/urls.get()', () => {
     assert.deepEqual(get(), {
       b6UTxQ: {
         longURL: "https://www.tsn.ca",
-        userID: "aJ48lW"
+        userID: "aJ48lW",
+        timesClicked: 0,
+        clientIds: [],
+        visitLog: [
+          {
+            clientID: 'abc',
+            date: 'June 10th 2017'
+          }
+        ]
       },
       i3BoGr: {
         longURL: "https://www.google.ca",
-        userID: "aJ48lW"
+        userID: "aJ48lW",
+        timesClicked: 0,
+        clientIds: [],
+        visitLog: [
+          {
+            clientID: 'abc',
+            date: 'June 10th 2017'
+          }
+        ]
       }
     });
   });
   it('should return single url object when passed a valid key', () => {
-    assert.deepEqual(get('i3BoGr'), {longURL: "https://www.google.ca", userID: "aJ48lW"});
+    assert.deepEqual(get('i3BoGr'), {
+      longURL: "https://www.google.ca",
+      userID: "aJ48lW",
+      timesClicked: 0,
+      clientIds: [],
+      visitLog: [
+        {
+          clientID: 'abc',
+          date: 'June 10th 2017'
+        }
+      ]
+    });
   });
   it('should return undefined when passed a key that doesnt exist', () => {
     assert.isUndefined(get('randomkey'));
